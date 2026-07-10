d1a3d5e5dfb710f8df4e6e0fbf61fbfde46e0671
Jennie Whittaker <jenniekwhittaker@gmail.com>
2026-07-10 21:17:52 +0100

    Add: Added the edit/delete urls to the urls.py file

--- tracker/urls.py
+++ tracker/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     # Map the root address of the app to the memorial_home view function
     path('', views.memorial_home, name="home"),
+    path('tribute/edit/<int:pk>/', views.edit_tribute, name='edit_tribute'),
+    path('tribute/delete/<int:pk>', views.delete_tribute, name='delete_tribute'),   # noqa: E501
 ]
